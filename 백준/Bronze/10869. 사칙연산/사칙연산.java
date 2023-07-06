62b3f16d105207de6f8d634499a5a35bbcaa8d99
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-07 08:37:02 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer str = new StringTokenizer(bufferedReader.readLine(), " ");
 
         int a = Integer.parseInt(str.nextToken());
         int b = Integer.parseInt(str.nextToken());
 
-        bufferedWriter.write(Integer.toString(a+b)+"\n");
-        bufferedWriter.write(Integer.toString(a-b)+"\n");
-        bufferedWriter.write(Integer.toString(a*b)+"\n");
-        bufferedWriter.write(Integer.toString(a/b)+"\n");
-        bufferedWriter.write(Integer.toString(a%b));
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.print(a%b);
     }
 }
