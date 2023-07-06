8a568a40a9d2e7ea5efb2abc70b5c3df1a0b620b
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-07 08:39:33 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,22 +1,17 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer str = new StringTokenizer(bufferedReader.readLine(), " ");
 
-        double a = Double.parseDouble(str.nextToken());
-        double b = Double.parseDouble(str.nextToken());
+        double a = Integer.parseInt(str.nextToken());
+        double b = Integer.parseInt(str.nextToken());
 
-        bufferedWriter.write(Double.toString(a/b));
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        System.out.print(a/b);
     }
 }
