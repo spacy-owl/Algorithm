657aa47333048a3c95cececd808a8e48103a90de
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-06 20:20:56 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufferedWriter.write("Hello World!");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
