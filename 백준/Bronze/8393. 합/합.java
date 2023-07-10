8e4b64def2b4d759cd82c988e113b39e1625d7b7
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-10 23:59:45 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+
+        long sum = a * (a + 1) / 2;
+
+        System.out.print(sum);
+    }
+}
