d7e05efeccf2c9373fb97da2a3e8472f5691fa04
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-14 20:14:16 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int max = 0;
+        int sum = 0;
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i = 0; i < n; i++) {
+           int a = Integer.parseInt(st.nextToken());
+           sum += a;
+            if (max < a) {
+                max = a;
+            }
+        }
+
+        System.out.print((float) sum/n/max*100);
+    }
+}
