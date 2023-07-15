b2e6503c316a1b8349ddb270df525e7d8bba8512
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-15 16:31:56 +0900

    [Bronze II] Title: 단어의 개수, Time: 264 ms, Memory: 20168 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        System.out.print(st.countTokens());
+    }
+}
