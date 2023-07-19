fe7e3ab3b6991b96ee46322d0216bb545a300d86
ttulgicoding <130344606+ttulgicoding@users.noreply.github.com>
2023-07-19 17:00:33 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 148.29 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = bufferedReader.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i<str.length(); i++) {
+            char w = str.charAt(i);
+            if (Character.isUpperCase(w)) {
+                sb.append(Character.toLowerCase(w));
+            } else {
+                sb.append(Character.toUpperCase(w));
+            }
+        }
+        System.out.print(sb);
+    }
+}
