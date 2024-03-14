a0513177d9b4397b6a5f4a51d18e2ebd9e3c8d75
spacy-owl <130344606+spacy-owl@users.noreply.github.com>
2024-03-14 15:41:21 +0900

    [Bronze III] Title: 홀수, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -2,14 +2,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int sum = 0;
         int min = 100;
 
         for (int i=0; i<7; i++) {
-            int k = Integer.parseInt(br.readLine());
+            int k =read();
             if (k%2 == 1) {
                 sum += k;
                 if (min > k) {
@@ -28,6 +27,10 @@ public class Main {
 
         bw.flush();
         bw.close();
-        br.close();
+    }
+    static int read() throws IOException{
+        int c,n = System.in.read() & 15;
+        while ((c=System.in.read()) > 47 && c < 58) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
